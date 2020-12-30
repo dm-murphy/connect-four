c6800efd78b3da0310791bbb5568d77055f9af3e
Daniel Murphy <danielmichaelmurphy@gmail.com>
2020-12-30 10:46:04 -0500

    Updated game_spec tests with comments to describe query and command messages not being tested

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,16 +6,32 @@ describe Game do
     # Public Script Method -> No test needed, but check methods inside
   end
 
+  describe '#swap_player' do
+    # Command sent to self so don't test?
+  end
+
   describe '#display_rules' do
     # Query sent to self? 
     # Puts message. No test needed.
   end
 
-  describe '#take_turn' do
-    # Public Script Method -> No test needed, but check methods inside
+  describe '#game_over?' do
+    # Outgoing query message sent to Board object, so don't test?
   end
 
-  describe '#swap_player' do
-    # Command sent to self so don't test?
+  describe '#tie' do
+    # Sends outgoing (query?) message to Board object, so don't test?
+    # Puts message sent to self, don't test
+  end
+
+  describe '#winner' do
+    # Sends outgoing (query?) message to Board object, so don't test?
+    # Puts message sent to self, don't test
+  end
+
+  describe '#take_turn' do
+    # Sends outgoing query message to Player object, don't test
+    # Sends outgoing query message to Board object, don't test
+    # Sends outgoing command message to Boad object, don't test
   end
 end

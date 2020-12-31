36eadd1ff1df65d6c6e56ffcfcefeabd89c68819
Daniel Murphy <danielmichaelmurphy@gmail.com>
2020-12-31 12:55:02 -0500

    Updated comments

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,17 +6,23 @@ describe Game do
     # Public Script Method -> No test needed, but check methods inside
   end
 
-  describe '#swap_player' do
-    # Command sent to self so don't test?
-  end
-
   describe '#display_rules' do
     # Query sent to self? 
     # Puts message. No test needed.
   end
 
+  describe '#take_turn' do
+    # Sends outgoing query message to Player object, don't test
+    # Sends outgoing query message to Board object, don't test
+    # Sends outgoing command message to Boad object, don't test
+  end
+
   describe '#game_over?' do
     # Outgoing query message sent to Board object, so don't test?
+  end
+
+  describe '#swap_player' do
+    # Command sent to self so don't test?
   end
 
   describe '#tie' do
@@ -27,11 +33,5 @@ describe Game do
   describe '#winner' do
     # Sends outgoing (query?) message to Board object, so don't test?
     # Puts message sent to self, don't test
-  end
-
-  describe '#take_turn' do
-    # Sends outgoing query message to Player object, don't test
-    # Sends outgoing query message to Board object, don't test
-    # Sends outgoing command message to Boad object, don't test
   end
 end
